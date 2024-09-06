54e8dc2a9c9803b48901b983be10fb6cfd03fdc1
arjpeg <araoudupi@gmail.com>
2024-09-06 18:48:43 -0500

    add file run support

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,39 @@
-use std::io::{self, Write};
+use std::{
+    env, fs,
+    io::{self, Write},
+};
+
+use owo_colors::OwoColorize;
 
 use helix::program::Program;
 
 fn main() {
-    match std::env::args().nth(1) {
-        Some(_filename) => todo!(),
+    match env::args().nth(1) {
+        Some(path) => run_file(&path),
         None => repl(),
+    }
+}
+
+fn run_file(path: &str) {
+    let content = match fs::read_to_string(path) {
+        Ok(content) => content,
+        Err(_) => {
+            eprintln!(
+                "{}: {}",
+                "Error".red().bold(),
+                format!("file path {path:?} does not exist").bold()
+            );
+
+            return;
+        }
+    };
+
+    let mut program = Program::new();
+    let main = program.add_source(path.to_owned(), content);
+
+    match program.run(main) {
+        Ok(_) => {}
+        Err(e) => program.pretty_print_error(e),
     }
 }
 
@@ -13,12 +41,11 @@ fn repl() {
     let mut program = Program::new();
 
     loop {
-        //let mut line = String::new();
-        // print!("helix > ");
+        let mut line = String::new();
+        print!("{} > ", "helix".green());
 
-        // io::stdout().flush().unwrap();
-        // io::stdin().read_line(&mut line).unwrap();
-        let line = "2 * 3".to_string();
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut line).unwrap();
 
         let main = program.add_source("<stdin>".to_string(), line);
 
@@ -26,6 +53,5 @@ fn repl() {
             Ok(value) => println!("{value}"),
             Err(e) => program.pretty_print_error(e),
         }
-        break;
     }
 }
